c4373c3791629c77ce3a3cc0a8d794a8defdd102
faisalAbdo11 <93899814+faisal0coding@users.noreply.github.com>
2024-08-27 15:22:40 +0300

    Final Keeper App for writing Notes without database

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <StickyNote2Icon />
+        Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
